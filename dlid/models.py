8729555b24b47313c9bb4485e4ce227cd0f588fa
Ivan Dolgushev <ivandolgushev@gmail.com>
2022-10-29 17:06:34 +0300

    Add ResNeXt block

--- dlid/models.py
+++ dlid/models.py
@@ -5,7 +5,7 @@ import torch
 from torch import nn, Tensor
 from torch.nn import functional as F
 
-__all__ = ['AlexNet']
+__all__ = ['AlexNet', 'VGG', 'Inception', 'GoogleNet', 'Residual', 'ResNet']
 
 
 class AlexNet(Classifier):
@@ -189,3 +189,124 @@ class GoogleNet(Classifier):
             Inception(256, (160, 320), (32, 128), 128),
             Inception(384, (192, 384), (48, 128), 128),
             nn.AdaptiveAvgPool2d((1, 1)), nn.Flatten())
+
+
+class Residual(nn.Module):
+    """The Residual block of ResNet.
+
+    Attributes:
+        num_channels: channels in a resnet block.
+        use_1x1conv: if number of chanels does not equal to the input chanels.
+        stride: stride to be used within resnet block.
+    """
+    def __init__(
+        self,
+        num_channels: int,
+        use_1x1conv: bool = False,
+        stride: int = 1
+    ):
+        super().__init__()
+        self.conv1 = nn.LazyConv2d(num_channels,
+                                   kernel_size=3,
+                                   stride=stride,
+                                   padding=1)
+
+        self.conv2 = nn.LazyConv2d(num_channels,
+                                   kernel_size=3,
+                                   padding=1)
+        if use_1x1conv:
+            self.conv3 = nn.LazyConv2d(num_channels,
+                                       kernel_size=1,
+                                       stride=stride)
+        else:
+            self.conv3 = None
+        self.bn1 = nn.LazyBatchNorm2d()
+        self.bn2 = nn.LazyBatchNorm2d()
+
+    def forward(self, X: Tensor):
+        Y = F.relu(self.bn1(self.conv1(X)))
+        Y = self.bn2(self.conv2(Y))
+        if self.conv3:
+            X = self.conv3(X)
+        Y += X
+        return F.relu(Y)
+
+
+class ResNet(Classifier):
+    """ The GoogLeNet model.
+
+    Attributes:
+        arch: list of tuples, describing net architecture.
+        lr: learning rate to use for training.
+        num_classes: number of classes to be used for prediction and in the
+        final fully-connected layer.
+        net: ResNet neural net architecture.
+    """
+    def __init__(
+        self,
+        arch: Tuple[Tuple[int]],
+        lr: float = 0.1,
+        num_classes: int = 10
+    ):
+        super(ResNet, self).__init__()
+        self.save_hyperparameters()
+        self.net = nn.Sequential(self.b1())
+
+        for i, b in enumerate(arch):
+            # *b  -> num_residiuals = b[0], num_channels = b[1]
+            self.net.add_module(f'b{i+2}',
+                                self.block(*b, first_block=(i == 0)))
+        self.net.add_module('last', nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)), nn.Flatten(),
+            nn.LazyLinear(num_classes)
+        ))
+
+    def b1(self):
+        return nn.Sequential(
+            nn.LazyConv2d(64, kernel_size=7, stride=2, padding=3),
+            nn.LazyBatchNorm2d(), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+    def block(self, num_residuals, num_channels, first_block=False):
+        blk = []
+        for i in range(num_residuals):
+            if i == 0 and not first_block:
+                blk.append(Residual(num_channels, use_1x1conv=True, stride=2))
+            else:
+                blk.append(Residual(num_channels))
+        return nn.Sequential(*blk)
+
+
+class ResNeXtBlock(nn.Module):
+    "The ResNeXt block."
+    def __init__(self, num_channels, groups,
+                 bot_mul, use_1x1conv=False,
+                 stride=1):
+        super().__init__()
+        bot_channels = int(round(num_channels * bot_mul))
+        self.conv1 = nn.LazyConv2d(bot_channels, kernel_size=1,
+                                   stride=1)
+        self.conv2 = nn.LazyConv2d(bot_channels, kernel_size=3,
+                                   stride=stride, padding=1,
+                                   groups=bot_channels//groups)
+        self.conv3 = nn.LazyConv2d(bot_channels, kernel_size=1, stride=1)
+
+        self.bn1 = nn.LazyBatchNorm2d()
+        self.bn2 = nn.LazyBatchNorm2d()
+        self.bn3 = nn.LazyBatchNorm2d()
+
+        if use_1x1conv:
+            self.conv4 = nn.LazyConv2d(num_channels, kernel_size=1,
+                                       stride=stride)
+            self.bn4 = nn.LazyBatchNorm2d()
+        else:
+            self.conv4 = None
+
+    def forward(self, X):
+        Y = F.relu(self.bn1(self.conv1(X)))
+        Y = F.relu(self.bn2(self.conv2(Y)))
+        Y = self.bn3(self.conv3(Y))
+        if self.conv4:
+            X = self.bn4(self.conv4(X))
+        return F.relu(Y + X)
